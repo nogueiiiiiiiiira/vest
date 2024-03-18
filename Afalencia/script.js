d7d3fdadc1b39338200fdd37a9de0d7e45c5ed09
nogueiiiiiiiiira <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2024-03-18 00:00:41 -0300

    Create script.js

---
+++ Afalencia/script.js
@@ -0,0 +1,34 @@
+function enredo1() {
+    document.getElementById("card1").style.display = "block";
+}
+
+
+function enredo2(){
+    document.getElementById("card2").style.display = "block";
+
+}
+
+function enredo3(){
+    document.getElementById("card3").style.display = "block";
+
+}
+
+function enredo4(){    
+    document.getElementById("card4").style.display = "block";
+
+}
+
+function enredo5(){
+    document.getElementById("card5").style.display = "block";
+
+}
+
+function enredo6(){
+    document.getElementById("card6").style.display = "block";
+
+}
+
+function enredo7(){
+    document.getElementById("card7").style.display = "block";
+
+}
